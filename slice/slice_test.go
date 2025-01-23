4f58f59828008a4d28f754da72c51b281e00e209
Michael Thaler <michael.thaler@dlr.de>
2025-01-23 17:43:34 +0100

    Updated slice_test.go.

--- slice/slice_test.go
+++ slice/slice_test.go
@@ -13,3 +13,11 @@ func TestFiler(t *testing.T) {
 	})
 	assert.Equal(t, []int{2, 4, 6}, s.slice)
 }
+
+func TestMap(t *testing.T) {
+	s := New[int]([]int{1, 2, 3, 4, 5, 6})
+	s.Map(func(i int) int {
+		return i * i
+	})
+	assert.Equal(t, []int{1, 4, 9, 16, 25, 36}, s.slice)
+}
