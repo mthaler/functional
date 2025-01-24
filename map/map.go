31efbe5071bb8d89f49aedadb84b90f6cbdca897
Michael Thaler <michael.thaler@dlr.de>
2025-01-24 07:53:43 +0100

    Updated map.go.

--- map/map.go
+++ map/map.go
@@ -8,12 +8,12 @@ func New[K comparable, V any](m map[K]V) MyMap[K, V] {
 	return MyMap[K, V]{mymap: m}
 }
 
-func (m *MyMap[K, V]) Filter(predicate func(K, V) bool) {
-	var result map[K]V
+func (m *MyMap[K, V]) Filter(predicate func(K, V) bool) MyMap[K, V] {
+	result := make(map[K]V)
 	for key, value := range m.mymap {
 		if predicate(key, value) {
 			result[key] = value
 		}
 	}
-	m.mymap = result
+	return New[K, V](result)
 }
