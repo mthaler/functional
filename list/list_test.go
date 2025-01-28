bb2232dab0ca6145907a46d437068e5d8e8521e8
Michael Thaler <michael.thaler@dlr.de>
2025-01-28 10:37:07 +0100

    Updated list_test.go.

--- list/list_test.go
+++ list/list_test.go
@@ -9,15 +9,9 @@ import (
 func TestFilter(t *testing.T) {
 	l := New(1, 2, 3, 4, 5, 6)
 	l2 := l.Filter(func(item any) bool {
-
 		i := item.(int)
 		return i%2 == 0
 	})
-	expected := New(2, 4, 6)
-	assert.Equal(t, expected, l2)
-}
-
-func TestToSlice(t *testing.T) {
-	l := New(1, 2, 3, 4, 5, 6)
-	assert.Equal(t, []any{1, 2, 3, 4, 5, 6}, l.ToSlice())
+	exptected := New(2, 4, 6)
+	assert.Equal(t, exptected, l2)
 }
