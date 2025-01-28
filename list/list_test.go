79a8702792f9814734260aa86984020b35abd046
Michael Thaler <michael.thaler@dlr.de>
2025-01-28 14:08:52 +0100

    Updated ist_test.go.

--- list/list_test.go
+++ list/list_test.go
@@ -12,6 +12,14 @@ func TestFilter(t *testing.T) {
 		i := item.(int)
 		return i%2 == 0
 	})
-	exptected := New(2, 4, 6)
-	assert.Equal(t, exptected, l2)
+	assert.Equal(t, New(2, 4, 6), l2)
+}
+
+func TestMap(t *testing.T) {
+	l := New(1, 2, 3, 4, 5, 6)
+	l2 := l.Map(func(i any) any {
+		j := i.(int)
+		return j * j
+	})
+	assert.Equal(t, New(1, 4, 9, 16, 25, 36), l2)
 }
