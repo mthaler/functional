ffd1b09d62d537181cfed15eff0f242d589a1c21
Michael Thaler <michael.thaler@dlr.de>
2025-01-28 12:50:43 +0100

    Updated list_test.go.

--- list/list_test.go
+++ list/list_test.go
@@ -1,15 +1,28 @@
 package mylist
 
 import (
+	"container/list"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestFilter(t *testing.T) {
 	l := New(1, 2, 3, 4, 5, 6)
-	l.Filter(func(item any) bool {
+	l2 := l.Filter(func(item any) bool {
 		i := item.(int)
 		return i%2 == 0
 	})
-	//exptected := New(2, 4, 6)
-	//assert.Equal(t, exptected, l2)
+	exptected := New(2, 4, 6)
+	assert.Equal(t, exptected, l2)
+}
+
+func toSlice(l MyList) []int {
+	var result []int
+	li := list.List(*l)
+	for e := li.Front(); e != nil; e = e.Next() {
+		v := e.Value
+		result = append(result, v)
+	}
+	return result
 }
